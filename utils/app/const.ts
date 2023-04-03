64ec66c56c30d4478d02039ea66a3df909802dac
SugarMoe <68399440+tangxiaojieblog@users.noreply.github.com>
2023-04-03 22:05:25 +0800

    Update const.ts

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_SYSTEM_PROMPT =
-  process.env.DEFAULT_SYSTEM_PROMPT || "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.";
+  process.env.DEFAULT_SYSTEM_PROMPT || "欢迎使用 JiangYou-ChatBot，仅供交流学习使用.";
 
 export const OPENAI_API_HOST =
   process.env.OPENAI_API_HOST || 'https://api.openai.com';
